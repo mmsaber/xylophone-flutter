68ca5cac2dbb1e0710521359d4ac9b93e131754e
mmsaber <mm.saber91@gmail.com>
2019-11-28 00:42:30 +0300

    commit changes

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,18 @@ void main() => runApp(XylophoneApp());
 class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black12,
         body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: listMyWidgets()),
         ),
       ),
     );
   }
+
   List<Widget> listMyWidgets() {
     List<MaterialColor> colors = new List();
     colors.add(Colors.blue);
@@ -28,14 +29,15 @@ class XylophoneApp extends StatelessWidget {
     colors.add(Colors.deepOrange);
 
     List<Widget> list = new List();
-    for(var i = 0 ; i < 7; i++){
-      list.add(new FlatButton(
-        child: Text('Mohamed'),
-        color: colors[i],
-        onPressed: () {
-          final AudioCache player = AudioCache();
-          player.play('note${i + 1}.wav');
-        },
+    for (var i = 0; i < 7; i++) {
+      list.add(Expanded(
+        child: new RaisedButton(
+          color: colors[i],
+          onPressed: () {
+            final AudioCache player = AudioCache();
+            player.play('note${i + 1}.wav');
+          },
+        ),
       ));
     }
 
