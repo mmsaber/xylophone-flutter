a52da20602ee4cd7ee78eaba3a6e73fb934238d6
mmsaber <mm.saber91@gmail.com>
2019-11-27 22:25:29 +0300

    applying some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: listMyWidgets()),
         ),
       ),
     );
+  }
+  List<Widget> listMyWidgets() {
+    List<MaterialColor> colors = new List();
+    colors.add(Colors.blue);
+    colors.add(Colors.teal);
+    colors.add(Colors.green);
+    colors.add(Colors.red);
+    colors.add(Colors.yellow);
+    colors.add(Colors.blueGrey);
+    colors.add(Colors.deepOrange);
+
+    List<Widget> list = new List();
+    for(var i = 0 ; i < 7; i++){
+      list.add(new FlatButton(
+        child: Text('Mohamed'),
+        color: colors[i],
+        onPressed: () {
+          final AudioCache player = AudioCache();
+          player.play('note${i + 1}.wav');
+        },
+      ));
+    }
+
+    return list;
   }
 }
